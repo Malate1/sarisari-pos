88b45cbbc0e843e058ef952f35a7892f92c64596
Michael Malate <malatemichael22@gmail.com>
2026-06-16 09:45:56 +0800

    s

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,6 +1,6 @@
 // src/components/LoginPage.jsx
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from './contexts/AuthContext';
 
 export default function LoginPage() {
   const { signIn, signUp, loading } = useAuth();
