56cbb5a3f88c9d62f4371c7137a3512e661eb73e
systpro04 <systemprogrammer04.com>
2026-06-16 08:58:09 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -565,7 +565,7 @@ export default function App() {
 					</div>
 					<div>
 						<h1 className="text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-						LM SariHubs
+						LM SariHub 2026
 						</h1>
 						<p className="text-xs text-gray-500 font-medium">
 						Online Store Manager v1.0
