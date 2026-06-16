ca222fe36410dbf7405cb7cba183c2a02b4e39a2
systpro04 <systemprogrammer04.com>
2026-06-16 09:03:10 +0800

    update

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,6 +1,5 @@
 // src/contexts/AuthContext.jsx
-import React, { createContext, useState, useEffect, useContext } from 'react';
-import { AuthProvider } from "./contexts/AuthContext";
+import React, { AuthProvider, useState, useEffect, useContext } from "react";
 import { db } from './db';
 import toast from 'react-hot-toast';
 
