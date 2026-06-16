195449cde985d5b1cc22e0ac5aea1ca987d675b9
systpro04 <systemprogrammer04.com>
2026-06-16 08:52:36 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AuthProvider } from './contexts/AuthContext';
+import AuthProvider  from './contexts/AuthContext';
 import ProtectedRoute from './ProtectedRoute';
 import App from './App.jsx';
 import './index.css';
